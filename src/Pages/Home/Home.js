ecf463d24b78da7b69526a4dc3fca66f958db490
proloypaul <proloyp23@gmail.com>
2021-10-18 00:26:25 +0600

    first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home section</h1>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
